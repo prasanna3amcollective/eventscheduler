1e2549eee65ba32a4956c08e904afb96ed1e3344
prasanna3amcollective <prasanna3amcollective@gmail.com>
2026-05-23 13:08:01 +0530

    streamlined the columns in recurrence template

---
+++ prisma/migrations/20260523073328_remove_isrecurring_from_recurrence_template/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isRecurring` on the `RecurrenceTemplate` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "RecurrenceTemplate" DROP COLUMN "isRecurring";
